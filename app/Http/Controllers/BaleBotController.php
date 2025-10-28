e63313e55e8c77f0976cd57936848b0dc4e4f43f
mohammad mahdi haghshenas <mhgorgab@gmail.com>
2025-10-28 11:55:12 +0330

    bug fixed

--- app/Http/Controllers/BaleBotController.php
+++ app/Http/Controllers/BaleBotController.php
@@ -53,11 +53,11 @@ class BaleBotController extends Controller
                     $minutes = null;
                     if (!$user->can('restore_any_minutes'))
                     {
-                        $minutes = Minutes::query()->where('typer_id', $user->id)->latest()->limit(5)->get();
+                        $minutes = Minutes::query()->where('typer_id', $user->id)->latest('id')->limit(5)->get();
                     }
                     else
                     {
-                        $minutes = Minutes::query()->latest()->limit(5)->get();
+                        $minutes = Minutes::query()->latest('id')->limit(5)->get();
                     }
 
                     if ($minutes->isEmpty()) {
