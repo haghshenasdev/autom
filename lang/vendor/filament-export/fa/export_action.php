ca7d3577f940233010cd369a7a5c4c41b5ebae6e
mh <mhgorgab@gmail.com>
2025-11-28 08:57:14 +0330

    bugs fixed

---
+++ lang/vendor/filament-export/fa/export_action.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'action_label' => 'خروجی گرفتن',
+    'modal_heading' => 'خروجی گرفتن',
+    'file_name_field_label' => 'نام فایل',
+    'format_field_label' => 'فرمت',
+    'page_orientation_field_label' => 'جهت صفحه',
+    'filter_columns_field_label' => 'فیلتر ستون‌ها',
+    'additional_columns_field' => [
+        'label' => 'ستون‌های اضافی',
+        'title_field_label' => 'عنوان',
+        'default_value_field_label' => 'مقدار پیش‌فرض',
+        'add_button_label' => 'افزودن ستون',
+    ],
+    'export_action_label' => 'خروجی گرفتن',
+    'print_action_label' => 'چاپ',
+    'preview_action_label' => 'پیش‌ نمایش',
+    'cancel_action_label' => 'انصراف',
+    'page_orientation_portrait' => 'عمودی',
+    'page_orientation_landscape' => 'افقی',
+];
+
---
+++ lang/vendor/filament-export/fa/table_view.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'preview_modal_heading' => 'پیشنمایش',
+    'print_action_label' => 'چاپ',
+    'export_action_label' => 'خروجی گرفتن',
+];
