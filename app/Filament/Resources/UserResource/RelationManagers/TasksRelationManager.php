6bb769c15d44ebb7eb183e6590f970b9c287b7d3
mohammad mahdi haghshenas <mhgorgab@gmail.com>
2025-12-21 10:22:25 +0330

    updated

---
+++ app/Filament/Resources/UserResource/RelationManagers/TasksRelationManager.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\RelationManagers;
+
+use App\Filament\Widgets\LatestTasks;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TasksRelationManager extends RelationManager
+{
+    protected static string $relationship = 'task_responsible';
+
+    protected static ?string $label = 'کار';
+
+    protected static ?string $pluralLabel = 'کار';
+
+    protected static ?string $modelLabel = 'کار';
+
+    protected static ?string $title = 'کار ها';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return (new LatestTasks)->table($table);
+        return $table
+            ->recordTitleAttribute('title')
+            ->columns([
+                Tables\Columns\TextColumn::make('title'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
