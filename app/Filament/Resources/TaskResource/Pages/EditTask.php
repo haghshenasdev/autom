4acbcb885b3ef42f3dfa8ec2c09014d8a78d3004
mohammad mahdi haghshenas <mhgorgab@gmail.com>
2026-01-06 09:54:06 +0330

    tst

--- app/Filament/Resources/TaskResource/Pages/EditTask.php
+++ app/Filament/Resources/TaskResource/Pages/EditTask.php
@@ -49,7 +49,7 @@ class EditTask extends EditRecord
                         // چون multiple هست، آرایه برمی‌گردد
                         foreach ($data['selected_result'] as $selected) {
                             [$modelType, $modelId] = explode('|', $selected);
-                            dd($livewire->form->get('project_id'));
+                            dd($livewire->form);
                             if ($modelType === \App\Models\Project::class) {
                                 // فقط روی فرم ست شود
                                 $livewire->form->fill([
