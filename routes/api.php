8f5b38ff231c2350c1c17d44f1b653d1a97117ba
haghdev <mhgorgab@outlook.com>
2026-07-26 11:17:37 +0330

    update

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\ProfileController;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,5 +35,17 @@ Route::middleware('auth:sanctum')->group(function () {
         '/get_avatar/{filename}',
         [ProfileController::class, 'get_avatar']
     );
+
+    Route::get('/appendix-other-show/{path}', function ($path) {
+        if (!Storage::disk('private_appendix_other')->exists($path)) {
+            abort(404);
+        }
+
+        $content = Storage::disk('private_appendix_other')->get($path);
+
+        return response($content, 200)
+            ->header('Content-Type', getMimeTypeFromExtension($path))
+            ->header('Content-Disposition', 'inline; filename="' . basename($path) . '"');
+    })->where('path', '.*');
 
 });
