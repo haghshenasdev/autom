bf45adcce18c6e4ea9cf809f63e0b1c80836fdcd
haghdev <mhgorgab@outlook.com>
2026-08-01 13:26:09 +0330

    u

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,10 +6,14 @@ use App\Models\Traits\HasStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
+use Rupadana\ApiService\Contracts\HasAllowedFilters;
+use Rupadana\ApiService\Contracts\HasAllowedSorts;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\QueryBuilder\AllowedFilter;
+use Illuminate\Database\Eloquent\Builder;
 
-class Project extends Model
+class Project extends Model implements HasAllowedSorts,HasAllowedFilters
 {
     use HasFactory,HasStatus,LogsActivity;
 
@@ -78,6 +82,45 @@ class Project extends Model
     {
         return LogOptions::defaults()->logExcept(['updated_at','created_at'])->logAll()->logOnlyDirty() // فقط وقتی مقدار تغییر کرد ذخیره بشه
         ->dontSubmitEmptyLogs(); // لاگ خالی ثبت نشه
+    }
+
+    public static function getAllowedSorts(): array
+    {
+        return [
+            'id',
+            'name',
+            'status',
+            'created_at',
+            'updated_at',
+        ];
+    }
+
+    public static function getAllowedFilters(): array
+    {
+        return [
+            'id',
+            'name',
+            'status',
+            'created_at',
+            'updated_at',
+            AllowedFilter::callback(
+                'search',
+                function (Builder $query, $value) {
+
+                    $query->where(function ($q) use ($value) {
+
+                        if (is_numeric($value)) {
+                            $q->orWhere('id', $value);
+                        }
+
+                        $q->orWhere('name', 'LIKE', "%{$value}%")
+                            ->orWhere('description', 'LIKE', "%{$value}%");
+
+                    });
+
+                }
+            ),
+        ];
     }
 
 }
