1a9eb3ed7549b9652cb6a3b4bd3bec8ed823145b
haghdev <mhgorgab@outlook.com>
2026-08-03 11:46:23 +0330

    u

--- app/Models/MinutesGroup.php
+++ app/Models/MinutesGroup.php
@@ -5,8 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Filament\Forms;
+use Rupadana\ApiService\Contracts\HasAllowedFilters;
+use Rupadana\ApiService\Contracts\HasAllowedSorts;
+use Illuminate\Database\Eloquent\Builder;
+use Spatie\QueryBuilder\AllowedFilter;
 
-class MinutesGroup extends Model
+class MinutesGroup extends Model implements HasAllowedSorts,HasAllowedFilters
 {
     use HasFactory;
 
@@ -33,6 +37,40 @@ class MinutesGroup extends Model
             Forms\Components\Select::make('parent_id')->label('زیر مجموعه')
                 ->relationship('parent', 'name')
                 ->searchable()->preload()
+        ];
+    }
+
+    public static function getAllowedSorts(): array
+    {
+        return [
+            'id',
+            'name',
+            'parent_id',
+        ];
+    }
+
+    public static function getAllowedFilters(): array
+    {
+        return [
+            'id',
+            'name',
+            'parent_id',
+            AllowedFilter::callback(
+                'search',
+                function (Builder $query, $value) {
+
+                    $query->where(function ($q) use ($value) {
+
+                        if (is_numeric($value)) {
+                            $q->orWhere('id', $value);
+                        }
+
+                        $q->orWhere('name', 'LIKE', "%{$value}%");
+
+                    });
+
+                }
+            ),
         ];
     }
 }
